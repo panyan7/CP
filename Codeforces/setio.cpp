252c521896fadc68710fef7db378c1b6c45ce973
panyan7 <ypan2@andrew.cmu.edu>
2024-03-27 20:05:12 -0400

    Update template with io

---
+++ Codeforces/setio.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pii pair<int,int>
+#define pll pair<long long,long long>
+
+int tt = 1, n, m, k;
+
+void setIO(string s) {
+	ios_base::sync_with_stdio(0); cin.tie(0); 
+	freopen((s+".in").c_str(),"r",stdin);
+	freopen((s+".out").c_str(),"w",stdout);
+}
+
+void solve() {
+}
+
+int main() {
+    setIO("");
+    // cin >> tt;
+    while (tt--) {
+        solve();
+    }
+    return 0;
+}
